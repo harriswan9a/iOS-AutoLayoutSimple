0c7077c1bfe521e548a946fcdd62f5648f39a0d7
harris.w <harriswan9a@gmail.com>
2016-11-26 17:18:36 +0800

    feat(init): first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Workshop: iOS Auto Layout
+
+![](https://raw.githubusercontent.com/harriswan9a/iOS-AutoLayoutSimple/master/screenshot/output.gif)
+
+![](https://raw.githubusercontent.com/harriswan9a/iOS-AutoLayoutSimple/master/20161126banner_large.png)
---
+++ testAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testAutoLayout
+//
+//  Created by 1500089A on 2016/11/26.
+//  Copyright © 2016年 Softmobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
